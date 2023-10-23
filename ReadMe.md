7e6cab630eb0962d7256241dda30817888cb9400
Siyuan Guo <67089757+syguo96@users.noreply.github.com>
2023-10-23 15:58:42 +0200

    add working example for quick start

--- ReadMe.md
+++ ReadMe.md
@@ -1,11 +1,42 @@
-### Usage
-The code here can be used to reproduce the results in the paper titled 
-Causal de Finetti: On the identification of invariant causal structure 
-in Exchangeable data. To reproduce the results in the article create a python 
-3.6+ environment, pip install the requirements.txt file. 
-Run the main.py file. 
+### Causal de Finetti: On the identification of invariant causal structure in Exchangeable data
 
-We acknowledge the use of the CD-NOD matlab code implemented in Huang et al. (2020)
-in our experiments. 
+This is the code repository for the paper
+_Causal de Finetti: On the identification of invariant causal structure 
+in Exchangeable data_ (NeurIPS 2023). 
+
+Performing Causal-de-Finetti algorithm on exchagneable data allows 
+one to uniquely identify the correct causal structure in bivariate and 
+multivariate settings. 
+
+To reproduce the results, pip install the `requirements.txt` file and 
+run the `main.py` file. The results are saved in `experiments/results` folder. 
+
+## Quick Start
+To run the causal de Finetti algorithm is as easy as running the following code snippet:
+
+```
+from src.models.causaldf import *
+from experiments.synthetic_data_generation import *
+
+num_env_bivariate = 1000
+num_env_multivariate = 5000
+num_sample_per_env = 2
+
+## Bivariate
+data = scm_bivariate_continuous(num_env = num_env_bivariate, num_sample = num_sample_per_env)
+estimate, _ = run_causaldf_bivariate(data)
+correct = True if estimate == data['true_structure'] else False
+
+print('Is correct in bivariate:', correct)
+
+## Multivariate
+data = scm_multivariate_binary(num_env = num_env_multivariate, num_sample = num_sample_per_env, num_var = 3)
+estimate, _ = run_causaldf_multivariate(data)
+correct = True if estimate == data['true_structure'] else False
+
+print('Is correct in multivariate:', correct)
+```
+
+
 
 
--- experiments/synthetic_data_generation.py
+++ experiments/synthetic_data_generation.py
@@ -3,48 +3,6 @@ from typing import Dict
 import networkx as nx
 import random
 
-def scm_bivariate_binary(seed: int, num_env: int) -> Dict:
-    """
-    Create Binary Exchangeable Data for Bivariate Graph
-    :param seed:
-    :param num_env:
-    :return:
-    """
-
-    # Instantiating the output results: data
-    data = {}
-
-    num_sample = 2
-    alpha_theta = 1
-    beta_theta = 3
-    alpha_psi = 2
-    beta_psi = 4
-
-    np.random.seed(seed)
-    theta = np.random.beta(alpha_theta, beta_theta, num_env)
-    psi = np.random.beta(alpha_psi, beta_psi, num_env)
-    # mode decides the true causal structure 0: X->Y; 1: Y->X; 2: X \ind Y
-    mode = np.random.choice(3)
-    if mode == 0:
-        X = np.random.binomial(1, p=theta, size=(num_sample, num_env))
-        psi_ind = np.random.binomial(1, p=psi, size=(num_sample, num_env))
-        Y = (psi_ind != X).astype(int)
-        data['true_structure'] = set([(0, 1)])
-    elif mode == 1:
-        Y = np.random.binomial(1, p=theta, size=(num_sample, num_env))
-        psi_ind = np.random.binomial(1, p=psi, size=(num_sample, num_env))
-        X = (psi_ind != Y).astype(int)
-        data['true_structure'] = set([(1, 0)])
-    elif mode == 2:
-        X = np.random.binomial(1, p=theta, size=(num_sample, num_env))
-        Y = np.random.binomial(1, p=psi, size=(num_sample, num_env))
-        data['true_structure'] = set([])
-
-
-    data['x'] = X
-    data['y'] = Y
-
-    return data
 
 def scm_bivariate_continuous(num_env: int, num_sample: int) -> Dict:
     """
@@ -155,48 +113,3 @@ def scm_multivariate_binary(num_env: int, num_sample: int, num_var: int) -> Dict
 
     return data
 
-def scm_multivariate_continuous(num_env: int, num_sample: int, num_var: int) -> Dict:
-    """
-    Create Binary Exchangeable Data for Bivariate Graph
-    :param seed:
-    :param num_env:
-    :return:
-    """
-
-    # Instantiating the output results: data
-    data = {}
-    data['data'] = {}
-
-    # Ensure there are specified num_var in the generated DAG
-    nodes_generated = []
-    while len(nodes_generated) != num_var or len(nodes_generated) == 0:
-        G = nx.gnp_random_graph(num_var, 0.5, directed=True)
-        DAG = nx.DiGraph([(u, v, {'weight': random.randint(-10, 10)}) for (u, v) in G.edges() if u < v])
-        nodes_generated = DAG.nodes
-
-    data['true_structure'] = set(DAG.edges)
-
-    N = np.random.uniform(-10, 10, (num_var, num_env))
-    N = np.repeat(N[:, :, np.newaxis], num_sample, axis=2)
-    Nprime = np.random.laplace(N)
-
-    A = np.eye(num_var)
-    for (v1, v2) in DAG.edges:
-        A[v2, v1] = np.random.randint(low=1, high=10)
-
-    D = np.einsum('ij, jkh->ikh', A, Nprime)
-    var_dict = {}
-    for v in range(num_var):
-        var_dict[str(v)] = D[v, :].T
-
-    data['data'] = var_dict
-    Data = D.reshape(num_var, -1).T
-    c_indx = np.repeat(range(1, num_env + 1), num_sample).reshape(-1, 1).astype(float)
-
-
-    data['cd-nod'] = {
-        'data': Data,
-        'c_indx': c_indx,
-        'true_structure': set(DAG.edges)
-    }
-    return data
